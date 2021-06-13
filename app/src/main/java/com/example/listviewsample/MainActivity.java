008de0b2ef8d13d1dbc782cdd21aba7b51381070
KoshiDra <koshidradev@gmail.com>
2021-06-13 12:26:42 +0900

    リスナとイベントハンドラ追加

--- app/src/main/java/com/example/listviewsample/MainActivity.java
+++ app/src/main/java/com/example/listviewsample/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.listviewsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv = findViewById(R.id.lvMenu);
+        lv.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // タップされた項目名を取得
+            String item = (String) parent.getItemAtPosition(position);
+
+            // トーストで表示する文字列を生成
+            String show = String.format("あなたが選んだ定食：%s", item);
+
+            // トーストの表示
+            Toast.makeText(MainActivity.this, show, Toast.LENGTH_LONG).show();
+        }
     }
 }
